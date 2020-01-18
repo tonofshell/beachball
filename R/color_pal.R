446e135d1ac7ad04127782325ac63bda20bc419f
Adam Shelton <aqs@uchicago.edu>
2020-01-18 12:48:42 -0600

    Tweaked existing color palettes, added "heatmap" palette option

--- R/color_pal.R
+++ R/color_pal.R
@@ -1,74 +1,95 @@
-# color_pal
+#' Beachball Color Palette
+#'
+#' Returns a color palette as a vector of RGB color hexcodes.
+#'
+#' @param n number of palette colors
+#' @param type palette type
+#' @param reverse reverses palette order
+#' @param levels number of colors to return
+#' @return A character vector of color hexcodes
+#' @importFrom grDevices colorRampPalette
+#' @export
 
-color_pal = function(n, type = "discrete", reverse = FALSE) {
+color_pal = function(n, type = "discrete", reverse = FALSE, levels = n) {
   #Discrete - distinct categories
-  #Segmented - two sided continuous with neutral center
+  #Segmented (Spectrum) - two sided continuous with neutral center
+  #(Soft Spectrum) - two sided continuous with neutral center but only changes shade
+  #(Monochrome)
   #Continuous - like segmented but w/o neutral center
-  names_mid = c("grey", "pale yellow", "purple", "dark grey", "pine green")
-  colors_mid = c("#C8DCC8", "#E6E673", "#A01EF0", "#5A5A5A", "#32AA41")
+  #Heatmap - continuous with changes from dark to light to accentuate "high" values
+  names_mid = c("grey", "yellow", "purple", "dark_grey", "pine_green", "bright_yellow")
+  colors_mid = c("#E6E6E6", "#E6D845", "#6D17E6", "#5A5A5A", "#2E9937", "#FFEA00")
 
-  order_main = c("mid offset", "light", "normal", "dark")
-  colors_warm = c("#FFD747", "#FF7837", "#EB5028", "#D73219")
-  colors_cool = c("#B1E651", "#1ED2D7", "#0FA0F5", "#0A55CD")
+  order_main = c("extra_light", "light", "normal", "dark", "alt_light")
+  colors_warm = c("#E69A17", "#E65C17", "#E61717", "#D417E6", "#E68673")
+  colors_cool = c("#5BCC28", "#17E6A1", "#17A1E6", "#5229CC", "#73E6E6")
 
   if (n > 7) {
     final = switch (type,
                     "discrete" = colorRampPalette(c(colors_warm[1], colors_warm[2], colors_warm[3], colors_mid[3], colors_cool[3], colors_cool[1]))(n),
                     "segmented" = colorRampPalette(c(colors_warm[4], colors_warm[3], colors_warm[2], colors_mid[1], colors_cool[2], colors_cool[3], colors_cool[4]))(n),
-                    "continuous" = colorRampPalette(c(colors_warm[3], colors_warm[2], colors_warm[1], colors_mid[2], colors_cool[1], colors_cool[2], colors_cool[3]))(n),
-                    "warm" = colorRampPalette(c(colors_mid[2], colors_warm, colors_mid[3]))(n),
-                    "cool" = colorRampPalette(c(colors_mid[2], colors_cool, colors_mid[3]))(n)
+                    "continuous" = colorRampPalette(c(colors_warm[4], colors_warm[3], colors_warm[2], colors_warm[1], colors_mid[2], colors_cool[1], colors_cool[2], colors_cool[3], colors_cool[4]))(n),
+                    "warm" = colorRampPalette(c(colors_warm, colors_mid[3]))(n),
+                    "cool" = colorRampPalette(c(colors_cool, colors_mid[3]))(n),
+                    "heatmap" = colorRampPalette(c(colors_mid[5], colors_cool[3], colors_mid[3], colors_warm[3], colors_mid[6]))(n)
     )
   } else {
     final = switch (as.character(n),
                     "1" = switch (type,
                                   "discrete" = colors_warm[3],
-                                  "segmented" = colors_mid[4],
-                                  "continuous" = colors_mid[3],
+                                  "segmented" = colors_mid[1],
+                                  "continuous" = colors_mid[2],
                                   "warm" = colors_warm[1],
-                                  "cool" = colors_cool[3]
+                                  "cool" = colors_cool[3],
+                                  "heatmap" = colors_warm[4]
                     ),
                     "2" = switch (type,
                                   "discrete" = c(colors_warm[3], colors_cool[3]),
                                   "segmented" = c(colors_warm[3], colors_cool[3]),
                                   "continuous" = c(colors_warm[3], colors_cool[3]),
                                   "warm" = c(colors_warm[2], colors_warm[3]),
-                                  "cool" = c(colors_cool[2], colors_cool[3])
+                                  "cool" = c(colors_cool[2], colors_cool[3]),
+                                  "heatmap" = c(colors_warm[4], colors_mid[6])
                     ),
                     "3" = switch (type,
                                   "discrete" = c(colors_warm[3], colors_cool[3], colors_warm[1]),
                                   "segmented" = c(colors_warm[3], colors_mid[1], colors_cool[3]),
-                                  "continuous" = c(colors_warm[3], colors_mid[3], colors_cool[3]),
+                                  "continuous" = c(colors_warm[3], colors_mid[2], colors_cool[3]),
                                   "warm" = c(colors_warm[1], colors_warm[2], colors_warm[3]),
-                                  "cool" = c(colors_cool[1], colors_cool[2], colors_cool[3])
+                                  "cool" = c(colors_cool[1], colors_cool[2], colors_cool[3]),
+                                  "heatmap" = c(colors_warm[4], colors_warm[3], colors_mid[6])
                     ),
                     "4" = switch (type,
                                   "discrete" = c(colors_warm[3], colors_cool[3], colors_warm[1], colors_mid[3]),
-                                  "segmented" = c(colors_warm[4], colors_warm[3], colors_mid[1], colors_cool[4]),
-                                  "continuous" = c(colors_warm[1], colors_warm[3], colors_mid[3], colors_cool[3]),
+                                  "segmented" = colorRampPalette(c(colors_warm[3], colors_mid[1], colors_cool[3]))(4),
+                                  "continuous" = c(colors_warm[3], colors_warm[2], colors_cool[2], colors_cool[3]),
                                   "warm" = c(colors_warm[1], colors_warm[2], colors_warm[3], colors_warm[4]),
-                                  "cool" = c(colors_cool[1], colors_cool[2], colors_cool[3], colors_cool[4])
+                                  "cool" = c(colors_cool[1], colors_cool[2], colors_cool[3], colors_cool[4]),
+                                  "heatmap" = c(colors_cool[3], colors_warm[4], colors_warm[3], colors_mid[6])
                     ),
                     "5" = switch (type,
                                   "discrete" = c(colors_warm[3], colors_cool[3], colors_warm[1], colors_mid[3], colors_cool[1]),
-                                  "segmented" = c(colors_warm[4], colors_warm[3], colors_mid[1], colors_cool[3], colors_cool[4]),
-                                  "continuous" = c(colors_warm[1], colors_warm[3], colors_mid[3], colors_cool[3], colors_mid[5]),
-                                  "warm" = c(colors_mid[2], colors_warm[1], colors_warm[2], colors_warm[3], colors_mid[3]),
-                                  "cool" = c(colors_mid[2], colors_cool[1], colors_cool[2], colors_cool[3], colors_mid[3])
+                                  "segmented" = c(colors_warm[3], colors_warm[2], colors_mid[1], colors_cool[2], colors_cool[3]),
+                                  "continuous" = c(colors_warm[3], colors_warm[2], colors_mid[2], colors_cool[2], colors_cool[3]),
+                                  "warm" = colors_warm,
+                                  "cool" = colors_cool,
+                                  "heatmap" = c(colors_mid[5], colors_cool[3], colors_mid[3], colors_warm[3], colors_mid[6])
                     ),
                     "6" = switch (type,
                                   "discrete" = c(colors_warm[3], colors_cool[3], colors_warm[1], colors_mid[3], colors_cool[1], colors_warm[2]),
-                                  "segmented" = c(colors_warm[4], colors_warm[3], colors_warm[2], colors_mid[1], colors_cool[3], colors_cool[4]),
+                                  "segmented" = colorRampPalette(c(colors_warm[3], colors_warm[2], colors_mid[1], colors_cool[2], colors_cool[3]))(6),
                                   "continuous" = c(colors_warm[3], colors_warm[2], colors_warm[1], colors_cool[1], colors_cool[2], colors_cool[3]),
-                                  "warm" = colorRampPalette(c(colors_mid[2], colors_warm[-4], colors_mid[3]))(6),
-                                  "cool" = colorRampPalette(c(colors_mid[2], colors_cool[-4], colors_mid[3]))(6)
+                                  "warm" = c(colors_warm, colors_mid[3]),
+                                  "cool" = c(colors_cool, colors_mid[3]),
+                                  "heatmap" = colorRampPalette(c(colors_mid[5], colors_cool[3], colors_mid[3], colors_warm[3], colors_mid[6]))(6)
                     ),
                     "7" = switch (type,
                                   "discrete" = colorRampPalette(c(colors_warm[1], colors_warm[2], colors_warm[3], colors_mid[3], colors_cool[3], colors_cool[1]))(7),
                                   "segmented" = c(colors_warm[4], colors_warm[3], colors_warm[2], colors_mid[1], colors_cool[2], colors_cool[3], colors_cool[4]),
                                   "continuous" = c(colors_warm[3], colors_warm[2], colors_warm[1], colors_mid[2], colors_cool[1], colors_cool[2], colors_cool[3]),
-                                  "warm" = colorRampPalette(c(colors_mid[2], colors_warm[-4], colors_mid[3]))(7),
-                                  "cool" = colorRampPalette(c(colors_mid[2], colors_cool[-4], colors_mid[3]))(7)
+                                  "warm" = colorRampPalette(c(colors_warm, colors_mid[3]))(7),
+                                  "cool" = colorRampPalette(c(colors_cool, colors_mid[3]))(7),
+                                  "heatmap" = colorRampPalette(c(colors_mid[5], colors_cool[3], colors_mid[3], colors_warm[3], colors_mid[6]))(7)
                     )
     )
   }
@@ -76,5 +97,5 @@ color_pal = function(n, type = "discrete", reverse = FALSE) {
   if (reverse) {
     return(rev(final))
   }
-  return(final)
+  return(colorRampPalette(final)(levels))
 }
